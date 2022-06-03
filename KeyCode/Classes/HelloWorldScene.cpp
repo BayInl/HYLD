ab935c8d0ff9ef393be575be09b88377fe31e363
SparkShi <Spark_Shi030212@163.com>
2022-06-04 01:20:30 +0800

    HelloWorldScene change. To try the Hero class.

--- KeyCode/Classes/HelloWorldScene.cpp
+++ KeyCode/Classes/HelloWorldScene.cpp
@@ -24,7 +24,14 @@
 
 #include "HelloWorldScene.h"
 
+//
+#include "Heroes.h"
+#include "AudioEngine.h"
+#include <stdio.h>
+//
+
 USING_NS_CC;
+using namespace cocos2d;
 
 Scene* HelloWorld::createScene()
 {
@@ -41,17 +48,17 @@ static void problemLoading(const char* filename)
 // on "init" you need to initialize your instance
 bool HelloWorld::init()
 {
-    //////////////////////////////
     // 1. super init first
     if ( !Scene::init() )
     {
         return false;
     }
 
+    //预加载
+    AudioEngine::preload("SkylightCut.mp3");
     auto visibleSize = Director::getInstance()->getVisibleSize();
     Vec2 origin = Director::getInstance()->getVisibleOrigin();
 
-    /////////////////////////////
     // 2. add a menu item with "X" image, which is clicked to quit the program
     //    you may modify it.
 
@@ -74,59 +81,185 @@ bool HelloWorld::init()
         closeItem->setPosition(Vec2(x,y));
     }
 
-    // create menu, it's an autorelease object
+    // 创建菜单
     auto menu = Menu::create(closeItem, NULL);
     menu->setPosition(Vec2::ZERO);
     this->addChild(menu, 1);
 
-    /////////////////////////////
     // 3. add your codes below...
+    
+    //增加一个角色的实例
+    this->addChild(hero_player);
+    hero_player->setPosition(Vec2(100, 200));
+    hero_player->Animater();//让它动起来
 
-    // add a label shows "Hello World"
-    // create and initialize a label
+    //增加骑士的大招武器
+    this->addChild(sword_bonus);
+    sword_bonus->setPosition(hero_player->getPosition()+Vec2(100,50));
+    sword_bonus->setVisible(false);
+    this->addChild(weapon_player);
+    weapon_player->setPosition(hero_player->getPosition() + Vec2(70, 30));
 
-    auto label = Label::createWithTTF("Hello World", "fonts/Marker Felt.ttf", 24);
-    if (label == nullptr)
-    {
-        problemLoading("'fonts/Marker Felt.ttf'");
-    }
-    else
-    {
-        // position the label on the center of the screen
-        label->setPosition(Vec2(origin.x + visibleSize.width/2,
-                                origin.y + visibleSize.height - label->getContentSize().height));
+    // 键盘事件监听
+   auto keyListener = EventListenerKeyboard::create();
+   keyListener->onKeyPressed = CC_CALLBACK_2(HelloWorld::onKeyPressedKnight, this);
+   _eventDispatcher->addEventListenerWithSceneGraphPriority(keyListener, this);
 
-        // add the label as a child to this layer
-        this->addChild(label, 1);
-    }
+   // 发射子弹
+   // 定义触摸事件的监听器。监听器有两种：
+   // 1.EventListenerTouchOneByOne：此类型对每个触摸事件调用一次回调方法。
+   // 2.EventListenerTouchAllAtOnce：此类型对所有的触摸事件调用一次回调方法。
+   auto mouseListener = EventListenerTouchOneByOne::create();
+   // 定义回调函数onTouchBegan():手指第一次碰到屏幕时被调用。
+   mouseListener->onTouchBegan = CC_CALLBACK_2(HelloWorld::onTouchBegan, this);
+   // 使用EventDispatcher来处理各种各样的事件，如触摸和其他键盘事件。
+   this->getEventDispatcher()->addEventListenerWithSceneGraphPriority(mouseListener, weapon_player);
 
-    // add "HelloWorld" splash screen"
-    auto sprite = Sprite::create("HelloWorld.png");
-    if (sprite == nullptr)
-    {
-        problemLoading("'HelloWorld.png'");
-    }
-    else
-    {
-        // position the sprite on the center of the screen
-        sprite->setPosition(Vec2(visibleSize.width/2 + origin.x, visibleSize.height/2 + origin.y));
+   //设置背景音乐
+   auto backgroundAudioID = AudioEngine::play2d("goldenslaughterer.mp3", true);
+   AudioEngine::resume(backgroundAudioID);
 
-        // add the sprite as a child to this layer
-        this->addChild(sprite, 0);
-    }
     return true;
 }
 
 
 void HelloWorld::menuCloseCallback(Ref* pSender)
 {
-    //Close the cocos2d-x game scene and quit the application
+    //关闭程序
     Director::getInstance()->end();
 
-    /*To navigate back to native iOS screen(if present) without quitting the application  ,do not use Director::getInstance()->end() as given above,instead trigger a custom event created in RootViewController.mm as below*/
-
-    //EventCustom customEndEvent("game_scene_close_event");
-    //_eventDispatcher->dispatchEvent(&customEndEvent);
+}
 
 
+//骑士专属键盘监听
+void HelloWorld::onKeyPressedKnight(EventKeyboard::KeyCode keyCode, Event* event) 
+{
+    log("Key with keycode %d pressed", keyCode);
+    switch (keyCode)  //操控方式：WSAD
+    {
+    case EventKeyboard::KeyCode::KEY_D: //按下D时角色为向右
+    {
+        auto moveBy = MoveBy::create(0.5f, Vec2(20, 0));
+        auto action = Sequence::create(moveBy, nullptr);
+        hero_player->setDirectRight();
+        weapon_player->setScaleX(1.0f);
+        sword_bonus->setScaleX(1.0f);
+        weapon_player->setPosition(hero_player->getPosition() + Vec2(70, 30));
+        sword_bonus->setPosition(hero_player->getPosition() + Vec2(100, 50));
+        hero_player->runAction(action);
+        weapon_player->runAction(action->clone());
+        sword_bonus->runAction(action->clone());
+        break;
+    }
+    case EventKeyboard::KeyCode::KEY_A: //按下A时角色为向左
+    {
+        auto moveBy = MoveBy::create(0.5f, Vec2(-20, 0));
+        auto action = Sequence::create(moveBy ,nullptr);
+        hero_player->setDirectLeft();
+        weapon_player->setScaleX(-1.0f);
+        sword_bonus->setScaleX(-1.0f);
+        weapon_player->setPosition(hero_player->getPosition()+Vec2(-70,30));
+        sword_bonus->setPosition(hero_player->getPosition() + Vec2(-100, 50));
+        hero_player->runAction(action);
+        weapon_player->runAction(action->clone());
+        sword_bonus->runAction(action->clone());
+        break;
+    }
+    case EventKeyboard::KeyCode::KEY_W:
+    {
+        auto moveBy = MoveBy::create(0.5f, Vec2(0, 20));
+        auto action = Sequence::create(moveBy, nullptr);
+        hero_player->runAction(action);
+        weapon_player->runAction(action->clone());
+        sword_bonus->runAction(action->clone());
+        break;
+    }
+    case EventKeyboard::KeyCode::KEY_S:
+    {
+        auto moveBy = MoveBy::create(0.5f, Vec2(0, -20));
+        auto action = Sequence::create(moveBy, nullptr);
+        hero_player->runAction(action);
+        weapon_player->runAction(action->clone());
+        sword_bonus->runAction(action->clone());
+        break;
+    }
+    case EventKeyboard::KeyCode::KEY_Q:  //大招
+    {
+        if (hero_player->getEnergy() >= ENERGYMAX_KNIGHT)
+        {
+            sword_bonus->setVisible(true);
+            weapon_player->setVisible(false);
+        }
+        else;
+        break;
+    }
+    case EventKeyboard::KeyCode::KEY_SHIFT:
+    {
+        if(hero_player->isDirectRight())
+        {
+            auto moveBy = MoveBy::create(0.5f, Vec2(120, 0));
+            auto action = Sequence::create(moveBy, nullptr);
+            hero_player->runAction(action);
+            weapon_player->runAction(action->clone());
+            sword_bonus->runAction(action->clone());
+        }
+        else
+        {
+            auto moveBy = MoveBy::create(0.5f, Vec2(-120, 0));
+            auto action = Sequence::create(moveBy, nullptr);
+            hero_player->runAction(action);
+            weapon_player->runAction(action->clone());
+            sword_bonus->runAction(action->clone());
+        }
+        break;
+    }
+    default:
+        break;
+    }
+}
+
+
+bool HelloWorld::onTouchBegan(Touch* touch, Event* unused_event) {
+    // 1 - Just an example for how to get the player object
+    // 说明一下作为第二个参数传递给addEventListenerWithSceneGraphPriority(eventListener, _player)的_player对象被访问的方式。
+    // auto node = unused_event->getcurrentTarget();
+
+    // 2.获取触摸点的坐标，并计算这个点相对于_player的偏移量。
+    Vec2 touchLocation = touch->getLocation();
+    Vec2 offset = touchLocation - hero_player->getPosition();
+
+    // 3.在玩家所在的位置创建一个子弹，将其添加到场景中。
+    auto projectile = Sprite::create("revolver_projectile.png");
+    projectile->setPosition(weapon_player->getPosition());
+    this->addChild(projectile);
+
+    // 4.将偏移量转化为单位向量，即长度为1的向量。
+    offset.normalize();
+    // 将其乘以1000，你就获得了一个指向用户触屏方向的长度为1000的向量。为什么是1000呢？因为长度应当足以超过当前分辨率下屏幕的边界。
+    auto shootAmount = offset * 1000;
+    // 将此向量添加到子弹的位置上去，这样你就有了一个目标位置。
+    auto realDest = shootAmount + projectile->getPosition();
+
+    // 5.创建一个动作，将子弹在2秒内移动到目标位置，然后将它从场景中移除。
+    auto actionMove = MoveTo::create(1.0f, realDest);
+    auto actionRemove = RemoveSelf::create();
+    projectile->runAction(Sequence::create(actionMove, actionRemove, nullptr));
+    if (weapon_player->isVisible())
+    {
+        weapon_player->attack();
+        hero_player->setEnergyOnePlus();
+    }
+    else if (hero_player->getEnergy() == 0)
+    {
+        sword_bonus->setVisible(false);
+        weapon_player->setVisible(true);
+        weapon_player->attack();
+        hero_player->setEnergyOnePlus();
+    }
+    else
+    {
+        sword_bonus->attack();
+        hero_player->setEnergy(0);
+    }
+    return true;
 }
--- KeyCode/Classes/HelloWorldScene.h
+++ KeyCode/Classes/HelloWorldScene.h
@@ -26,19 +26,39 @@
 #define __HELLOWORLD_SCENE_H__
 
 #include "cocos2d.h"
+#include "Heroes.h"
 
 class HelloWorld : public cocos2d::Scene
 {
 public:
     static cocos2d::Scene* createScene();
+    virtual bool init();    
 
-    virtual bool init();
-    
+    //建立玩家操控的角色类实例
+    //Wizard* hero_player= Wizard::create();
+    //Scientist* hero_player = Scientist::create();
+    Knight* hero_player = Knight::create();
+    //Berserker* hero_player = Berserker::create();
+
+    //建立武器类实例
+    Weapons* sword_bonus = hero_player->bindWeapon(1);
+    Weapons* weapon_player = hero_player->bindWeapon(0);
+    //SwordBonus* sword_bonus = SwordBonus::create();
+
+    //键盘监听，触碰监测
+    void onKeyPressedKnight(EventKeyboard::KeyCode keyCode, Event* event);
+    bool onTouchBegan(cocos2d::Touch* touch, cocos2d::Event* unused_event);
+
     // a selector callback
     void menuCloseCallback(cocos2d::Ref* pSender);
     
     // implement the "static create()" method manually
     CREATE_FUNC(HelloWorld);
+
+    cocos2d::Vector<cocos2d::SpriteFrame*> getAnimation(const char* format, int count);
+
 };
 
 #endif // __HELLOWORLD_SCENE_H__
+
+
